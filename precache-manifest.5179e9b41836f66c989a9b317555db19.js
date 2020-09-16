9727914cf2947e08c9be7c96c52bceb0ab750444
OsamaRashad88 <osamarashad_1988@yahoo.com>
2020-09-16 18:36:13 +0200

    Players React

---
+++ precache-manifest.5179e9b41836f66c989a9b317555db19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c568a85df48b7923ff5dedcf2d355375",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e1feb92a6148ff342f6",
+    "url": "./static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2dc9f33c102c7391e1a8",
+    "url": "./static/css/main.849d56c8.chunk.css"
+  },
+  {
+    "revision": "8e1feb92a6148ff342f6",
+    "url": "./static/js/2.f7bc3dc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f7bc3dc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc9f33c102c7391e1a8",
+    "url": "./static/js/main.43523507.chunk.js"
+  },
+  {
+    "revision": "5ef6d8813cd93a866081",
+    "url": "./static/js/runtime-main.489edbb0.js"
+  }
+]);
